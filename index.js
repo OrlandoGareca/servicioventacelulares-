13861c9e62068b78c357f218d0c03f29d6c51f53
root <orlando.dilmar.gareca.pena@gmail.com>
2018-08-19 23:13:51 -0400

    añadiendo funciones

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose')
 
 const Product = require('./models/product')
 
-
 const app = express()
 const port = process.env.PORT || 7777
 
@@ -13,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 // `...`
 
-app.get('/api/product' , (req,res) => {
+app.get('/api/product' ,(req,res) => {
    Product.find({} , (err, products) => {
     if (err) return res.status(500).send({ message:` Error al realizar la peticion: ${err}`})
     if(!products) return res.status(404).send({message: `No existen productos`})
@@ -30,9 +29,7 @@ app.get('/api/product/:productId' , (req,res) => {
         if(!product) return res.status(404).send({message: `El producto no existe`})
 
         res.status(200).send({product: product})
-
     })
-
 })
 
 app.post('/api/product' , (req, res) => {
